a6f76f46742fe02713579ded1ed0d382542ad842
Vaermi <dorisdrye@hotmail.com>
2025-11-01 20:06:53 +0100

    add initial ui and dummy data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,65 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using MongoDB.Driver;
-using SteamRandomizer.src.Services;
-using System.IO;
-using System.Text;
+﻿using SteamRandomizer.src.Model;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Path = System.IO.Path;
 
 namespace SteamRandomizer;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
-{
+public partial class MainWindow : Window {
 
-    public MainWindow()
-    {
+    private bool isFavorited = false;
+
+    List<Game> games = new List<Game> {
+            new Game { Id = 1, Title = "Portal 2", Genre = "Puzzle, Co-op", ImagePath = "Images/portal2.jpg" },
+            new Game { Id = 2, Title = "Hades", Genre = "Rogue-like, Action", ImagePath = "Images/hades.jpg" },
+            new Game { Id = 3, Title = "Dark Souls", Genre = "Souls-Like", ImagePath = "Images/dark-souls.jpg" },
+            new Game { Id = 4, Title = "Elden Ring", Genre = "Souls-Like", ImagePath = "Images/elden-ring.jpg" }
+    };
+
+    public MainWindow() {
         InitializeComponent();
+        
+    }
+
+    private void RandomButton_Click(object sender, RoutedEventArgs e) {
+        ContentControl content = (ContentControl)sender;
+        var game = this.ShuffleGames();
+        if (game != null) {
+            GameTitle.Text = game.Title;
+            GameGenre.Text = game.Genre;
+            //GameImage.Source = new BitmapImage(new Uri(Path.GetFullPath(game.ImagePath)));
+        }
+    }
+
+    private void FavoriteButton_Click(object sender, RoutedEventArgs e) {
+        isFavorited = !isFavorited;
+
+        if (isFavorited) {
+            FavoriteButton.Content = "✅ Gespeichert";
+            FavoriteButton.Background = Brushes.Gold;
+            FavoriteButton.Foreground = Brushes.Black;
+        }
+        else {
+            FavoriteButton.Content = "⭐ Favorit";
+            FavoriteButton.ClearValue(Button.BackgroundProperty);
+            FavoriteButton.ClearValue(Button.ForegroundProperty);
+        }
+    }
+
+    private void FilterButton_Click(object sender, RoutedEventArgs e) {
 
     }
 
+    private Game ShuffleGames(){
+        if (games == null || games.Count == 0) {
+            return null;
+        }
+            
+        Random rand = new Random();
+        int randomIndex = rand.Next(games.Count);
+        return games[randomIndex];
+    }
 }
---
+++ src/Model/Game.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SteamRandomizer.src.Model
+{
+    public class Game
+    {
+        //[BsonId]
+        //public ObjectId MongoId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
